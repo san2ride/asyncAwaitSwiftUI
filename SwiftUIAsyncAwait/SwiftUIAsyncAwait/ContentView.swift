770016f37626cdf272f33b66ec6ddb6debc43904
Jason <sanchezjason@me.com>
2024-02-06 11:48:51 -0700

    swiftui/async_await-new file

---
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwait/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIAsyncAwait
+//
+//  Created by don't touch me on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwait/SwiftUIAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncAwaitApp.swift
+//  SwiftUIAsyncAwait
+//
+//  Created by don't touch me on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
