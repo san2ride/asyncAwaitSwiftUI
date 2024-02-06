aff5351d618563c456afbdc92819b1eee840e0ae
Jason <sanchezjason@me.com>
2024-02-06 14:48:30 -0700

    swiftui/async_await/MVVM-WebService

--- SwiftUIAsyncAwait/SwiftUIAsyncAwait/ContentView.swift
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwait/ContentView.swift
@@ -20,19 +20,6 @@ struct ContentView: View {
     
     @State private var currentDates: [CurrentDate] = []
     
-    // fuction must be async
-    private func getDate() async throws -> CurrentDate? {
-        
-        guard let url = URL(string:
-            "https://ember-sparkly-rule.glitch.me/current-date") else {
-            fatalError("Webiste broken!")
-        }
-        // URLSession must be labeld await
-        let (data, _) = try await URLSession.shared.data(from: url)
-        // try? insures any issues with decoding, returns nil
-        return try? JSONDecoder().decode(CurrentDate.self, from: data)
-    }
-    
     private func populateDates() async {
         do {
             guard let currentDate = try await getDate() else {
---
+++ SwiftUIAsyncAwait/SwiftUIAsyncAwait/WebService.swift
@@ -0,0 +1,24 @@
+//
+//  WebService.swift
+//  SwiftUIAsyncAwait
+//
+//  Created by don't touch me on 2/6/24.
+//
+
+import Foundation
+
+class WebService {
+    
+    // fuction must be async
+    private func getDate() async throws -> CurrentDate? {
+        
+        guard let url = URL(string:
+            "https://ember-sparkly-rule.glitch.me/current-date") else {
+            fatalError("Webiste broken!")
+        }
+        // URLSession must be labeld await
+        let (data, _) = try await URLSession.shared.data(from: url)
+        // try? insures any issues with decoding, returns nil
+        return try? JSONDecoder().decode(CurrentDate.self, from: data)
+    }
+}
